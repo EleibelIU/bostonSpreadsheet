c40aa931c0355280f48026d386fc4561615d7ae9
Elizabeth Leibel <liz.leibel@yahoo.com>
2025-02-05 01:33:24 -0600

    old theme

--- map.js
+++ map.js
@@ -52,11 +52,12 @@ function initMap() {
 
     const map = L.map('map').setView(palladiumCoords, 10);
 
+    // CartoDB Dark Matter theme - no API key required
     L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
-        attribution: '©OpenStreetMap, ©CartoDB',
-        subdomains: 'abcd'
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+        subdomains: 'abcd',
+        maxZoom: 20
     }).addTo(map);
-
     // Add Palladium marker
     L.marker(palladiumCoords, { icon: palladiumIcon })
         .bindPopup('<strong>The Palladium</strong><br>Worcester Venue')
